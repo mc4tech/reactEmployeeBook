8499d66810b01fe4ca4965366bb28c3325e5aa89
mc4tech <chavezmeguel@yahoo.com>
2019-02-13 22:40:48 -0500

    updated dependencies, middleware and added user schema

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const flash = require('connect-flash');
+const session = require('express-session');
+const passport = require('passport');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -11,6 +14,9 @@ const db = process.env.MONGODB_URI || 'mongodb://localhost/reactEmployeeBook';
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
