1cf1cb4653125959cdaa0e2229f5995e33b14891
mc4tech <chavezmeguel@yahoo.com>
2019-02-14 20:22:00 -0500

    hash password to be saved in db

--- routes/users.js
+++ routes/users.js
@@ -14,13 +14,22 @@ router.post('/register', (req, res) => {
     email,
     password
   });
-  newUser
-    .save()
-    .then(user => {
-      console.log('user posted');
-      res.json(user);
+  // Hash Password
+  bcrypt.genSalt(10, (err, salt) =>
+    bcrypt.hash(newUser.password, salt, (err, hash) => {
+      if (err) throw err;
+      // set password to hashed
+      newUser.password = hash;
+      // save user
+      newUser
+        .save()
+        .then(user => {
+          req.flash('success_msg', 'You are now registered and can login');
+          res.json(user);
+        })
+        .catch(err => console.log(err));
     })
-    .catch(err => console.log(err));
+  );
 });
 
 module.exports = router;
