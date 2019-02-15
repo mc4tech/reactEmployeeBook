fbf768c0f410481807ae271ae9c2d0350d736a3d
mc4tech <chavezmeguel@yahoo.com>
2019-02-14 20:12:47 -0500

    passport & express session middleware

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const passport = require('passport');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+// Passport config
+require('./config/passport')(passport);
+
 // Mongo DB
 const db = process.env.MONGODB_URI || 'mongodb://localhost/reactEmployeeBook';
 
@@ -15,13 +18,37 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
-// Bodyparser
-app.use(express.urlencoded({ extended: false }));
-
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
+
+// Bodyparser
+app.use(express.urlencoded({ extended: false }));
+
+// Express Session Middleware
+app.use(
+  session({
+    secret: 'secret',
+    resave: true,
+    saveUninitialized: true
+  })
+);
+
+// Passport middleware
+app.use(passport.initialize());
+app.use(passport.session());
+
+// connect flash
+app.use(flash());
+
+// Global Vars
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash('success_msg');
+  res.locals.error_msg = req.flash('error_msg');
+  res.locals.error = req.flash('error');
+  next();
+});
 
 // Routes
 app.use('/', require('./routes/index'));
